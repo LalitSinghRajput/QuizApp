e7dcbe93b7ede0b82ffc498df2b9c8e5a20efff3
Lalit <lalitsr24@gmail.com>
2022-07-28 11:33:16 +0530

    added category selection screen

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var category: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.decorView.apply {
+            systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+
+        science_card.setOnClickListener {
+            category = "science"
+            Toast.makeText(this, "category: "+category, Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+}
